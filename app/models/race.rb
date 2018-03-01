a8b5d11f7c7cefe26bde90cd0acc9ceae01fbb46
Pavlo Voitsekhivskyi <voitseh@gmail.com>
2018-03-01 12:18:11 +0800

    Added geocoder

--- app/models/race.rb
+++ app/models/race.rb
@@ -5,7 +5,8 @@ class Race < ApplicationRecord
   has_many :users, through: :orders
   has_many :routes
 
-
   validates :name, :distance, :category, :date, :location, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 
 end
