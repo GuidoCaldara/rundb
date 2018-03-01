8b2ab6b258689b3f32a5fd82846bbfa5d69c4ca3
guido caldara <guido.caldara@gmail.com>
2018-03-01 11:22:35 +0800

    routes ok

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,3 +1,11 @@
 class Race < ApplicationRecord
   belongs_to :organisation
+  has_many :orders
+  has_many :reviews
+  has_many :users, through: :orders
+  has_many :routes
+
+
+  validates :name, :distance, :category, :date, :location, presence: true
+
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,3 +1,3 @@
 class Route < ApplicationRecord
-  belongs_to :user
+  belongs_to :race
 end
