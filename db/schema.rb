186d4338fed54303b22f9ea82e14c7f9e3a3f3e3
guido caldara <guido.caldara@gmail.com>
2018-03-08 22:57:26 +0800

    added lat and long to organizers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308033819) do
+ActiveRecord::Schema.define(version: 20180308144214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,10 +96,10 @@ ActiveRecord::Schema.define(version: 20180308033819) do
     t.float "date_stamp"
     t.integer "race_avg_rate"
     t.string "video"
+    t.string "photo"
     t.float "route_rate_avg"
     t.float "organisation_rate_avg"
     t.float "value_for_money_avg"
-    t.string "photo"
     t.integer "race_distance"
     t.index ["organisation_id"], name: "index_races_on_organisation_id"
   end
