e7fba0abbd0622d8dd5c9be63ab6050a76a47fa2
guido caldara <guido.caldara@gmail.com>
2018-03-09 15:53:57 +0800

    ok

--- db/migrate/20180309071813_delete_coordinates_to_organisations.rb
+++
@@ -1,7 +0,0 @@
-class DeleteCoordinatesToOrganisations < ActiveRecord::Migration[5.1]
-  def change
-      remove_column :organisations, :latitude
-      remove_column :organisations, :longitude
-
-  end
-end
